61b22facdc8edcb2049963a9ab56bd045031220d
Uthoai Marma <uthoaimarma597@gamil.com>
2024-01-22 01:13:03 +0600

    initial commit

---
+++ app/src/main/java/com/top/best/ecommerce/bd/auction/bazar/view/start/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.top.best.ecommerce.bd.auction.bazar.view.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.top.best.ecommerce.bd.auction.bazar.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
